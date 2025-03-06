2ca101c457cc01eafbbce5400212db042090b247
RIYA SHUKLA <114848829+riya0785@users.noreply.github.com>
2025-03-06 11:39:13 +0530

    Update test_generate_text.py

--- test_generate_text.py
+++ test_generate_text.py
@@ -4,7 +4,7 @@ from generate_text import generate_random_text
 def test_generate_random_text():
     result = generate_random_text()
     assert len(result) == 6
-    assert result.isalpha()  # Ensure only letters
+    assert result.isalpha()  #**Ensure only letters
 
 if __name__ == "__main__":
     pytest.main()
